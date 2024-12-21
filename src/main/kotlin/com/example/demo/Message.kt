415938d48a6cf95bb8624022decdfc1044874792
Carlos Henrique da Silva Barros Jr <carlosjunior@unimedrj.coop.br>
2024-12-21 19:14:33 -0300

    alterado projeto para usar o CrudRepository ao invés do Jdbc

--- src/main/kotlin/com/example/demo/Message.kt
+++ src/main/kotlin/com/example/demo/Message.kt
@@ -1,5 +1,9 @@
 package com.example.demo
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
 // data class é definido com o "data" no ínicio da declaração da classe
 // Estamos definindo a estrutura de uma classe com ID podendo ser nul mas text não pode ser nulo
-data class Message(val id: String?, val text: String)
+@Table("MESSAGES")
+data class Message(val text: String, @Id val id: String? = null )
